09eb14766bd7cbe09a1708e06b05c5082d077953
Ashwin <81082301+snoofox@users.noreply.github.com>
2022-10-27 23:01:38 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import random
+import json
+import re
+
+REGEX = "https://mega\.nz/((folder|file)/([^#]+)#(.+)|#(F?)!([^!]+)!(.+))"
+
+
+def mega_checker(url: str) -> bool:
+    if not bool(re.search(REGEX, url)):
+        return False
+
+    if url.split("/")[3] == "folder":
+        payload = {'a': 'f', 'c': 1, 'r': 1, 'ca': 1}
+    else:
+        payload = {'a': 'g', 'p': id}
+
+    sequence_num = random.randint(0, 0xFFFFFFFF)
+    link_id = url.split("/")[4].split("#")[0]
+
+    response = requests.post(
+        f"https://g.api.mega.co.nz/cs?id={sequence_num}&n={link_id}",
+        data=json.dumps([payload])
+    ).json()
+
+    if not type(response) is int:
+        return True
+    return False
+
+
+# print(mega_checker("https://mega.nz/folder/OHQlaAT#y_pYR8b96o14ZKK8NxZRyA"))
